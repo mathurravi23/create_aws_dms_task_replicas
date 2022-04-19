6d94d3f538091c5b74fdc8ae2db2d03b7d73bcb5
mathurravi23 <82545117+mathurravi23@users.noreply.github.com>
2022-04-18 20:56:41 -0500

    Update create_dms_task_replicas.py

--- create_dms_task_replicas.py
+++ create_dms_task_replicas.py
@@ -1,5 +1,8 @@
 import json
 import boto3
+from boto3 import Session
+from botocore.config import Config
+import os
 import sys
 import argparse
 
@@ -13,8 +16,8 @@ parser.add_argument('--target_source_endpoint', type=str, help = 'Specify ARN of
 parser.add_argument('--target_target_endpoint', type=str, help = 'Specify ARN of target endpoint in target region. For cross region move,make sure to specify target_region argument.')
 parser.add_argument('--migration_type', type=str, help = 'Specify new migration type for new dms task. Valid values: full-load OR cdc OR full-load-and-cdc')
 parser.add_argument('--counter', type=int, help = 'Specify START counter for new tasks')
-args = parser.parse_args()
 
+args = parser.parse_args()
 
 def main():
    try:
@@ -24,12 +27,12 @@ def main():
 
       response = client.describe_replication_tasks(Filters=[ { 'Name': 'replication-task-id', 'Values': [args.task_name] }])
 
-      with open("/tmp/tmp_dms_task_settings.json",'w') as f:
+      with open("tmp_dms_task_settings.json",'w') as f:
             f.write(response["ReplicationTasks"][0]["ReplicationTaskSettings"])
 
-      with open('/tmp/tmp_dms_task_settings.json') as f:
+      with open('tmp_dms_task_settings.json') as f:
             data = json.load(f)
-
+      
 
       newjson= '{'
       newjson= newjson + '"TargetMetadata":'+str(data["TargetMetadata"])
@@ -43,10 +46,10 @@ def main():
       newjson= newjson +'}'
 
 
-      with open("/tmp/new_dms_tasksettings.json", 'w') as f:
-                   f.write(newjson)
+      with open("new_dms_tasksettings.json", 'w') as f:
+                   f.write(newjson)  
 
-      if(args.target_dms_instance !=None):
+      if(args.target_dms_instance !=None):  
         dms_instance=args.target_dms_instance
       else:
         dms_instance=response["ReplicationTasks"][0]["ReplicationInstanceArn"]
@@ -66,6 +69,8 @@ def main():
       else:
         migration_type=response["ReplicationTasks"][0]["MigrationType"]
 
+
+
       if(args.counter !=None):
         i=args.counter
       else:
@@ -79,11 +84,11 @@ def main():
         trg_client= boto3.client('dms',region_name=args.target_region)
       else:
         trg_client= boto3.client('dms',region_name=args.region)
-
+     
 
       while j < icopy :
         j+=1
-        newtask=args.task_name+str(i)
+        newtask=args.task_name+str(i)  
         createresponse=trg_client.create_replication_task(
                                   ReplicationTaskIdentifier=newtask,
                                   SourceEndpointArn=source_endpoint,
@@ -99,7 +104,18 @@ def main():
       print ('')
       print ('')
       print('Script Completed !')
+      
+      print('Starting Cleanup of temp files..')
+
+      if os.path.exists("tmp_dms_task_settings.json"):
+        os.remove("tmp_dms_task_settings.json")
+
+      if os.path.exists("new_dms_tasksettings.json"):
+        os.remove("new_dms_tasksettings.json")
+      
       print('')
+      print('Cleanup complete!')
+
 
    except Exception as e:
     print(e)
